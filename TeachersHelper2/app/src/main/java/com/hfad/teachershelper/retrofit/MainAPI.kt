f7b7a0b6197bb8247401bd004ac0d5c2c4c21d17
AlinaGafurova10 <alina.gafurova.e2010@gmail.com>
2025-09-24 22:59:29 +0500

    изменить url

--- TeachersHelper2/app/src/main/java/com/hfad/teachershelper/retrofit/MainAPI.kt
+++ TeachersHelper2/app/src/main/java/com/hfad/teachershelper/retrofit/MainAPI.kt
@@ -42,7 +42,8 @@ interface MainAPI {
     ): MessageResponse
 
     companion object {
-        const val BASE_URL = "http://10.0.2.2:8000/"
+    //    const val BASE_URL = "http://10.0.2.2:8000/"
+        const val BASE_URL = "//http://127.0.0.1:8000/"
     }
 
 
